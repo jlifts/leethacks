263e8985e5c7857bc6699957d593e7a991d20e58
Josh Burgess <burgessj247@gmail.com>
2024-01-09 08:35:38 -0500

    adds validate sudoku #36

--- pkgs/arrays/medium.go
+++ pkgs/arrays/medium.go
@@ -1,1 +1,52 @@
 package arrays
+
+// Utilize bucket sort for O(n) time complexity, size of the array is equal to the size of hashmap
+func topKFrequent(nums []int, k int) []int {
+	m := make(map[int]int)
+
+	for _, v := range nums {
+		m[v]++
+	}
+
+	bucket := make([][]int, len(nums)+1)
+
+	for i, v := range m {
+		bucket[v] = append(bucket[v], i)
+	}
+
+	answer := make([]int, 0, k)
+
+	for i := len(bucket) - 1; i >= 0; i-- {
+		for _, v := range bucket[i] {
+			if k > 0 {
+				answer = append(answer, v)
+				k--
+			}
+		}
+	}
+
+	return answer
+}
+
+func productExceptSelf(nums []int) []int {
+	answer := make([]int, len(nums))
+	prefix := 1
+
+	for i := 0; i < len(nums); i++ {
+		answer[i] = prefix
+		prefix *= nums[i]
+	}
+
+	postfix := 1
+	for i := len(nums) - 1; i >= 0; i-- {
+		answer[i] *= postfix
+		postfix *= nums[i]
+	}
+
+	return answer
+}
+
+func isValidSudoku(board [][]byte) bool {
+
+	return false
+}
---
+++ pkgs/arrays/medium_test.go
@@ -0,0 +1,103 @@
+package arrays
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_topKFrequent(t *testing.T) {
+	type args struct {
+		nums []int
+		k    int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{
+			name: "Case 1",
+			args: args{
+				nums: []int{1, 1, 1, 2, 2, 3},
+				k:    2,
+			},
+			want: []int{1, 2},
+		},
+		{
+			name: "Case 2",
+			args: args{
+				nums: []int{1, 1, 1, 2, 2, 3, 4, 4, 4},
+				k:    3,
+			},
+			want: []int{1, 4, 2},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := topKFrequent(tt.args.nums, tt.args.k); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("topKFrequent() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_productExceptSelf(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{
+			name: "Case 1",
+			args: args{
+				nums: []int{1, 2, 3, 4},
+			},
+			want: []int{24, 12, 8, 6},
+		},
+		{
+			name: "Case 2",
+			args: args{
+				nums: []int{-1, 1, 0, -3, 3},
+			},
+			want: []int{0, 0, 9, 0, 0},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := productExceptSelf(tt.args.nums); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("productExceptSelf() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_isValidSudoku(t *testing.T) {
+	type args struct {
+		board [][]byte
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "Case 1",
+			args: args{
+				board: [][]byte{
+					{'5', '3', '.', '.', '7', '.', '.', '.', '.'}, {'6', '.', '.', '1', '9', '5', '.', '.', '.'}, {'.', '9', '8', '.', '.', '.', '.', '6', '.'}, {'8', '.', '.', '.', '6', '.', '.', '.', '3'}, {'4', '.', '.', '8', '.', '3', '.', '.', '1'}, {'7', '.', '.', '.', '2', '.', '.', '.', '6'}, {'.', '6', '.', '.', '.', '.', '2', '8', '.'}, {'.', '.', '.', '4', '1', '9', '.', '.', '5'}, {'.', '.', '.', '.', '8', '.', '.', '7', '9'},
+				},
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isValidSudoku(tt.args.board); got != tt.want {
+				t.Errorf("isValidSudoku() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
